ddc3d8956b9225b6cd6f9ecc216f7d98d2ae19a3
JSanchez78657 <joel.sanchez78657@gmail.com>
2021-12-07 09:30:00 -0800

    GUI side wasn't working, fixed now!

--- src/main/java/com/mpi/Starter.java
+++ src/main/java/com/mpi/Starter.java
@@ -1,10 +1,12 @@
 package com.mpi;
 
 import com.mpi.gui.GUI;
+import com.mpi.model.GUIListener;
 
 public class Starter {
     public static void main(String[] args) {
         GUI gui = new GUI();
+        GUIListener listener = new GUIListener(gui);
         gui.display();
     }
 }
